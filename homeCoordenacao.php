66f3d1a0fc51c0b4579f3c4d86242a28bd11d9fe
Ranyer1999 <ranyerpinheiro@gmail.com>
2025-09-16 20:02:47 -0400

    Update homeCoordenacao.php

--- homeCoordenacao.php
+++ homeCoordenacao.php
@@ -17,22 +17,23 @@ $termo  = $_POST['termo'] ?? '';
         <nav class="menu">
             <img src="/public/img/logo.png" alt="logo" id="logo">
             <ul>
-                <li><button onclick="window.location.href='loginSgi.php'">Sair</button></li>
+                <li id="sair"><button onclick="window.location.href='logout.php'">Sair</button></li>
             </ul>
         </nav>
 </header>
 
 <body>
-    <h2>Home</h2>
-    <form action="post" class="login-form">
+    <h2 id="h2-home">Home</h2>
+    <form action="post" class="perfil-form">
         Meu Perfil
     </form>
-    <form action="post" class="login-form">
+    <form action="post" class="dados-form">
         <a href="">Home</a>
         <a href="">Turmas</a>
         <a href="">Historico</a>
         <a href="">Candidaturas</a>
-        <a href="">Docentes</a>
+        <a href="cadastrarDocentes.php">Docentes</a>
+        <a href="listarDocentes.php">Listar Docentes</a>
     </form>
 </body>
 
